7eb4f1ddec6292e951af05135cac96b0320cbc26
fdzul <felipe.dzul.m@gmail.com>
2020-02-20 20:11:52 -0600

    This commit add the document the dataset

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A dataset containing the clinical dengue cases by AGEB and year (2008-2016)
 #' from nine location of Mexico.
 #'
-#' @format A data frame with 2174 rows and 12 variables:
+#' @format A sf object with 2174 rows and 12 variables:
 #' \describe{
 #'   \item{CVEGEO}{the ids }
 #'   \item{loc}{the name of locality}
@@ -21,3 +21,20 @@
 #' }
 #' @source \url{https://osf.io/8bjy5/}
 "den_loc_mex"
+
+#' Zika cases by municipality in the departament of Tolima, Colombia.
+#'
+#' A dataset containing the zika cases by AGEB in the years 2015 & 2016
+#' Tolima departament.
+#'
+#' @format A sf object with 46 rows and 6 variables:
+#' \describe{
+#'   \item{NAME_0}{the country.}
+#'   \item{NAME_1}{the name of departament.}
+#'   \item{NAME_2}{the name of municipality.}
+#'   \item{ZIKV_1516}{number of zika cases in 2015 & 2016 year.}
+#'   \item{geometry}{simple feature.}
+#'   ...
+#' }
+#' @source \url{https://f1000research.com/articles/5-568/v1#DS0}
+"ajmrzik16_col"
